b8d981ec2dcdf8188ce423048b43ee695d50a968
Reya Taylor <adamsreya@gmail.com>
2023-09-09 20:50:17 -0400

    worked on some components

---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './SearchBar.css'
+
+function Searchbar() {
+    return (
+        <div className="jamming">
+            <input></input>
+            <button>Search</button>
+        </div>
+    );
+}
+
+export default Searchbar; 
---
+++ src/Track/Track.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Track.css';
+
+
+function Track() {
+    return (
+        <div className="track">
+           
+            <h2>Title</h2>
+            <p>Artist Name</p>
+            
+            
+        </div>
+    )
+}
+
+export default Track;
